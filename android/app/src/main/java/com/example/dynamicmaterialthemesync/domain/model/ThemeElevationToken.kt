53e6b62a11f8bc756113ee809ea1691602a53812
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 15:54:01 -0700

    Auto commit generated Android file: app/src/main/java/com/example/dynamicmaterialthemesync/domain/model/ThemeElevationToken.kt

---
+++ android/app/src/main/java/com/example/dynamicmaterialthemesync/domain/model/ThemeElevationToken.kt
@@ -0,0 +1,9 @@
+package com.example.dynamicmaterialthemesync.domain.model
+
+/**
+ * Elevation values in dp mapped from Figma tokens.
+ */
+data class ThemeElevationToken(
+    val level: String,
+    val value: Float
+)
