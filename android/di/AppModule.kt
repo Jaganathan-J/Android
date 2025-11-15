db8b071ee8fa5aa62d2a3dbe013f601a6fa83a1c
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 02:29:03 -0700

    Auto commit generated Android file: di/AppModule.kt

---
+++ android/di/AppModule.kt
@@ -0,0 +1,56 @@
+package com.example.material3showcase.di
+
+import com.example.material3showcase.data.remote.api.ConfigApi
+import com.example.material3showcase.data.repository.ConfigRepositoryImpl
+import com.example.material3showcase.domain.repository.ConfigRepository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://example.com") // Replace with real base URL
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideConfigApi(retrofit: Retrofit): ConfigApi = retrofit.create(ConfigApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideConfigRepository(api: ConfigApi): ConfigRepository = ConfigRepositoryImpl(api)
+}
