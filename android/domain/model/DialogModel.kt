fdf29fb42b6e157eedb9b00ef809a8569696ca12
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 02:11:24 -0700

    Auto commit generated Android file: domain/model/DialogModel.kt

---
+++ android/domain/model/DialogModel.kt
@@ -0,0 +1,9 @@
+package com.example.material3showcase.domain.model
+
+data class DialogModel(
+    val id: String,
+    val title: String,
+    val message: String,
+    val confirmLabel: String,
+    val dismissLabel: String
+)
