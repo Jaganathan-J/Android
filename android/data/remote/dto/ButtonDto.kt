b885597c5510677a2e63657f2a240a22fb516ef2
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 02:11:27 -0700

    Auto commit generated Android file: data/remote/dto/ButtonDto.kt

---
+++ android/data/remote/dto/ButtonDto.kt
@@ -0,0 +1,8 @@
+package com.example.material3showcase.data.remote.dto
+
+data class ButtonDto(
+    val label: String?,
+    val style: String?,
+    val enabled: Boolean?,
+    val icon: String?
+)
