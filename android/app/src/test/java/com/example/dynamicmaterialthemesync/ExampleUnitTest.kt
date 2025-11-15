5affbace304b6f5acdec4354675093bce88b8b61
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 15:54:15 -0700

    Auto commit generated Android file: app/src/test/java/com/example/dynamicmaterialthemesync/ExampleUnitTest.kt

---
+++ android/app/src/test/java/com/example/dynamicmaterialthemesync/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.dynamicmaterialthemesync
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
