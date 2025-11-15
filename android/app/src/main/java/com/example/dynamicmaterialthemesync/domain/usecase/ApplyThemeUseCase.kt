1a832a085c38940a30f7138f4f895398fa5ec913
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 13:10:11 -0700

    Auto commit generated Android file: app/src/main/java/com/example/dynamicmaterialthemesync/domain/usecase/ApplyThemeUseCase.kt

---
+++ android/app/src/main/java/com/example/dynamicmaterialthemesync/domain/usecase/ApplyThemeUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.dynamicmaterialthemesync.domain.usecase
+
+import com.example.dynamicmaterialthemesync.domain.model.ThemePreferences
+import com.example.dynamicmaterialthemesync.domain.repository.PreferencesRepository
+import javax.inject.Inject
+
+class ApplyThemeUseCase @Inject constructor(
+    private val preferencesRepository: PreferencesRepository
+) {
+    suspend operator fun invoke(newPrefs: ThemePreferences) {
+        preferencesRepository.updateThemePreferences { newPrefs }
+    }
+}
