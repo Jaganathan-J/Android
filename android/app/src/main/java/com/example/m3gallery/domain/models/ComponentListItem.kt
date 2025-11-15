686504673af5aacffdd7c5c6f6678a902e6553ca
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 04:02:17 -0700

    Auto commit generated Android file: app/src/main/java/com/example/m3gallery/domain/models/ComponentListItem.kt

---
+++ android/app/src/main/java/com/example/m3gallery/domain/models/ComponentListItem.kt
@@ -0,0 +1,10 @@
+package com.example.m3gallery.domain.models
+
+/**
+ * Represents a fake component description item to populate lists.
+ */
+data class ComponentListItem(
+    val id: String,
+    val title: String,
+    val description: String
+)
