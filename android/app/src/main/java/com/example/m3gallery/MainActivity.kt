0ddcf1c82f6a050fa27f159724a298dc1a934163
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-15 03:43:29 -0700

    Auto commit generated Android file: app/src/main/java/com/example/m3gallery/MainActivity.kt

---
+++ android/app/src/main/java/com/example/m3gallery/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.m3gallery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.m3gallery.presentation.navigation.AppNavigation
+import com.example.m3gallery.presentation.ui.theme.M3GalleryTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            M3GalleryTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AppNavigation(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
