fa597c21b1da6857c70a785bfb3c1339f231525d
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-11-17 22:11:17 -0500

    Auto commit generated Android file: app/src/main/java/com/example/dynamicmaterialthemesync/DynamicMaterialThemeSyncApp.kt

---
+++ android/app/src/main/java/com/example/dynamicmaterialthemesync/DynamicMaterialThemeSyncApp.kt
@@ -0,0 +1,7 @@
+package com.example.dynamicmaterialthemesync
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DynamicMaterialThemeSyncApp : Application()
