95908073c022e05dae33aca5f0d3804d1eb439f2
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-01 01:18:19 -0500

    Auto commit generated Android file: app/src/main/java/com/luxemarket/app/presentation/theme/Color.kt

---
+++ android/app/src/main/java/com/luxemarket/app/presentation/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.luxemarket.app.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// LuxeMarket Core Palette
+val Charcoal = Color(0xFF121212)
+val Gold = Color(0xFFD4AF37)
+val OffWhite = Color(0xFFFAFAFA)
+val LightGray = Color(0xFFE0E0E0)
+val ErrorRed = Color(0xFFB00020)
+
+val DarkSurface = Color(0xFF1E1E1E)
+val GoldLight = Color(0xFFEACD68)
