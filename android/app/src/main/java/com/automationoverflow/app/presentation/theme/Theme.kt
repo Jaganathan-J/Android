7e86fe5607709ef2ece1c0bd1720fb07f1832563
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-03 07:53:08 -0500

    Auto commit generated Android file: app/src/main/java/com/automationoverflow/app/presentation/theme/Theme.kt

---
+++ android/app/src/main/java/com/automationoverflow/app/presentation/theme/Theme.kt
@@ -0,0 +1,27 @@
+package com.automationoverflow.app.presentation.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Violet700,
+    secondary = Cyan400,
+    background = AppBackground,
+    surface = AppSurface,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.White,
+    onSurface = Color.White
+)
+
+@Composable
+fun AutomationOverflowTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = DarkColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
