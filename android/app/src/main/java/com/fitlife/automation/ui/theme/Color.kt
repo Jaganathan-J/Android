fe057dcc3d4073e8ea15d286dd9d13260dbc358b
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-03 00:54:19 -0500

    Auto commit generated Android file: app/src/main/java/com/fitlife/automation/ui/theme/Color.kt

---
+++ android/app/src/main/java/com/fitlife/automation/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.fitlife.automation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// FitLife Dark Palette
+val MidnightBlue = Color(0xFF0A0A1A)
+val DarkSurface = Color(0xFF14142B)
+val NeonPurple = Color(0xFF7F00FF)
+val NeonPink = Color(0xFFE100FF)
+val TextWhite = Color(0xFFFFFFFF)
+val TextGrey = Color(0xFFA0A0B0)
+val SuccessGreen = Color(0xFF00C853)
+val ErrorRed = Color(0xFFFF453A)
