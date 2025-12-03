8864486a237f3a530973dec08e3997e5115b0b8e
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-03 07:32:36 -0500

    Auto commit generated Android file: app/src/main/java/com/example/fitlife/ui/theme/Color.kt

---
+++ android/app/src/main/java/com/example/fitlife/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.fitlife.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundDark = Color(0xFF0A0A16)
+val SurfaceDark = Color(0xFF14142B)
+val PrimaryPurple = Color(0xFF8A2BE2)
+val PrimaryBlue = Color(0xFF4169E1)
+val TextWhite = Color(0xFFFFFFFF)
+val TextGrey = Color(0xFFA0A3BD)
+val AccentGreen = Color(0xFF00E676)
+val AccentRed = Color(0xFFFF5252)
+val AccentYellow = Color(0xFFFFD740)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
