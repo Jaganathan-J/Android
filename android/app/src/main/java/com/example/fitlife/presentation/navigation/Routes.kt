d95daf3d517031856173cc4f5a794b9b2001eae5
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-03 00:47:01 -0500

    Delete android directory

--- android/app/src/main/java/com/example/fitlife/presentation/navigation/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fitlife.presentation.navigation
-
-sealed class Screen(val route: String) {
-    object Splash : Screen("splash")
-    object Login : Screen("login")
-    object Dashboard : Screen("dashboard")
-    object AddTransaction : Screen("add_transaction")
-    object TransactionsList : Screen("transactions_list")
-}
