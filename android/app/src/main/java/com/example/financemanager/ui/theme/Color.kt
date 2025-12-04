3beda242ad4f0b709638b22b2c55f66998628941
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-04 09:50:30 -0500

    Auto commit generated Android file: app/src/main/java/com/example/financemanager/ui/theme/Color.kt

---
+++ android/app/src/main/java/com/example/financemanager/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.financemanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF0A0A14)
+val SurfaceColor = Color(0xFF141422)
+val PrimaryBlue = Color(0xFF3B82F6)
+val PrimaryPurple = Color(0xFF8B5CF6)
+val TextWhite = Color(0xFFFFFFFF)
+val TextGrey = Color(0xFF9CA3AF)
+val InputBackground = Color(0xFF1F1F2E)
+val InputBorder = Color(0xFF2D2D3F)
+val SuccessGreen = Color(0xFF10B981)
+val ErrorRed = Color(0xFFEF4444)
