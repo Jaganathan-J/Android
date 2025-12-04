bc9cb670964d63f6a4314e09a4ba266c10bd500f
Jaganathan jagadeesan <jaganathan.j89@gmail.com>
2025-12-04 11:38:07 -0500

    Auto commit generated Android file: app/src/main/java/com/example/autoflow/ui/theme/Color.kt

---
+++ android/app/src/main/java/com/example/autoflow/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.autoflow.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundDark = Color(0xFF0A0A14)
+val SurfaceDark = Color(0xFF14142B)
+val PrimaryPurple = Color(0xFF7F00FF)
+val SecondaryBlue = Color(0xFF007FFF)
+val TextGrey = Color(0xFFA0A0B0)
+val SuccessGreen = Color(0xFF00C853)
